eb38bd98fb73fa4600c1226fba3f745e3c25c3ee
Pamela Mori <pam.cdm@gmail.com>
2016-04-12 21:28:16 -0300

    Redo line

--- .bash_profile
+++ .bash_profile
@@ -1,5 +1,5 @@
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+  . $(brew --prefix)/etc/bash_completion
 fi
 
 export PROMPT_COMMAND='__git_ps1 "\[\033[0;31m\]\u\[\033[0m\]@\[\033[0;34m\]\h\[\033[0;36m\] \w\[\033[0m\]" "\n\[\033[32m\]»\[\033[0m\] "'
--- setup.sh
+++ setup.sh
@@ -1,18 +1,7 @@
-check_and_install_bash_completion() {
-  if ! brew info bash-completion > /dev/null 2>&1 ; then
-    echo "Bash completion not installed"
-    echo "Installing it."
-    return 1
-  fi
-
-  echo "Bash completion already installed. Using your setup"
-  return 0
-} 
-
 bash_profile() {
 	echo "**** bash profile configuration ****"
   brew install bash-completion
-	cp .bash_profile ~/
+  cp .bash_profile ~/.bash_profile
   source ~/.bash_profile
 }
 
