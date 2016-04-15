d620f1952f44adbe8aa3a034e159be25cddfd988
Pamela Mori <pam.cdm@gmail.com>
2016-04-14 22:31:08 -0300

    Clean up

--- setup_dot_files.sh
+++ setup_dot_files.sh
@@ -1,24 +1,24 @@
-bash_profile() {
-	echo "**** bash profile configuration ****"
-  brew install bash-completion
-  cp .bash_profile ~/.bash_profile
-  source ~/.bash_profile
-}
-
 git_configuration() {
-  echo "**** configure git "
-  cp .gitconfig ~/  
+  echo "* configure git "
+	brew install git
+  cp .gitconfig ~/
 }
 
 git_ignore_configuration() {
-  echo "**** configure gitignore"
+  echo "* configure gitignore"
   git config --global core.excludesfile ~/.gitignore_global
   cp -a .gitignore ~/.gitignore_global
 }
 
+bash_configuration() {
+	echo "* bash profile configuration ****"
+  brew install bash-completion
+  cp .bash_profile ~/
+}
+
 vim_configuration() {
-  echo "**** configure vim"
-  rm -rf ~/.vim/
+  echo "* configure vim"
+	rm -rf ~/.vim/
 
   echo "install bundle"
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
@@ -28,18 +28,20 @@ vim_configuration() {
 
   echo "install colors"
   mkdir ~/.vim/colors
+
   rm -rf ~/Downloads/vim-monokai
   git clone git@github.com:sickill/vim-monokai.git ~/Downloads/vim-monokai
-  cp ~/Downloads/vim-monokai/colors/monokai.vim ~/.vim/colors/
+  sudo cp ~/Downloads/vim-monokai/colors/monokai.vim ~/.vim/colors/
 
   echo "install plugins"
   vim +PluginInstall +qall
 }
 
-setup() {
+setup_dot_files() {
   git_configuration
   git_ignore_configuration
-  vim_configuration
+	bash_configuration
+	vim_configuration
 }
 
-setup
+setup_dot_files
